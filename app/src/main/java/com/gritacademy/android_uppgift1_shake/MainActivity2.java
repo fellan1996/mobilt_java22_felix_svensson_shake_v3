080b77ac9e6b48400c1c596eb5689a763d2a4310
Felix Svensson <felixsvensson1996@gmail.com>
2023-08-30 14:57:52 +0200

    Initial commit

---
+++ app/src/main/java/com/gritacademy/android_uppgift1_shake/MainActivity2.java
@@ -0,0 +1,120 @@
+package com.gritacademy.android_uppgift1_shake;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 400;
+    private FragmentManager fm;
+    private shakyFragment shakyFragmentInstance;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button activityBtn = findViewById(R.id.activityBtn);
+
+        fm = getSupportFragmentManager();
+        shakyFragmentInstance = new shakyFragment();
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        fm.beginTransaction()
+                .add(R.id.frameLayout, BlankFragment.class, null) // gets the first animations
+                .commit();
+
+        activityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Jadååå", Toast.LENGTH_LONG).show();
+                Intent A1 = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(A1);
+            }
+        });
+    }
+
+    protected void onPause() {
+
+        super.onPause();
+
+        senSensorManager.unregisterListener(this);
+
+    }
+
+    protected void onResume() {
+
+        super.onResume();
+
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+            TextView sensorDataInfo = findViewById(R.id.sensordataInfo);
+
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+//                    Toast.makeText(MainActivity2.this, "Jadååå", Toast.LENGTH_LONG).show();
+                    fm.beginTransaction()
+                            .replace(R.id.frameLayout, shakyFragmentInstance, null) // gets the first animations
+                            .commit();
+//                    ______________Jag fick inte koden undertill till att fungera________________
+//                    shakyFragmentInstance.updateText("Latest score: " + Math.round(speed));
+
+                } else {
+                    fm.beginTransaction()
+                            .replace(R.id.frameLayout, BlankFragment.class, null) // gets the first animations
+                            .commit();
+
+                }
+                last_x = x;
+                last_y = y;
+                last_z = z;
+                float lessDecimalsX = Math.round(last_x * 10000);
+                float lessDecimalsY = Math.round(last_x * 10000);
+                float lessDecimalsZ = Math.round(last_x * 10000);
+                sensorDataInfo.setText(" X: " + lessDecimalsX / 10000 + "   Y: " + lessDecimalsY / 10000 + "   Z: " + lessDecimalsZ / 10000);
+            }
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
---
+++ app/src/main/java/com/gritacademy/android_uppgift1_shake/shakyFragment.java
@@ -0,0 +1,73 @@
+package com.gritacademy.android_uppgift1_shake;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link shakyFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class shakyFragment extends Fragment {
+
+    private TextView fragmentText1;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public shakyFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment shakyFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static shakyFragment newInstance(String param1, String param2) {
+        shakyFragment fragment = new shakyFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_shaky, container, false);
+
+        fragmentText1 = rootView.findViewById(R.id.fragmentText1);
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+    public void updateText(String newText) {
+        fragmentText1.setText(newText);
+    }
+}
